1fdc5045872b180dc612ae78f818dae78cf70af0
rcsfrontier <55377645+rcsfrontier@users.noreply.github.com>
2021-06-09 09:37:19 +0900

    Initial Commit For GitHub Actions GKE

---
+++ src/main/java/com/example/code/githubactionsgke/GithubActionsGkeApplication.java
@@ -0,0 +1,13 @@
+package com.example.code.githubactionsgke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsGkeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionsGkeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/code/githubactionsgke/GithubActionsGkeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.code.githubactionsgke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsGkeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
